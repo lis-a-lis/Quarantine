8dbebc021f9063ce54a9a029dbab7b75fa1d24f6
lis <lalislisa790@gmail.com>
2025-04-19 14:49:16 +0300

    Loading screen animation

--- Assets/_Quarantine/Code/UI/Loading/LoadingScreen.cs
+++ Assets/_Quarantine/Code/UI/Loading/LoadingScreen.cs
@@ -1,7 +1,6 @@
-﻿using DG.Tweening;
-using TMPro;
+﻿using TMPro;
+using DG.Tweening;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace _Quarantine.Code.UI.Loading
@@ -12,7 +11,8 @@ namespace _Quarantine.Code.UI.Loading
         [SerializeField] private Image _gearImage;
         [SerializeField] private Image _gearImage2;
         [SerializeField] private Image _gearImage3;
-        [SerializeField] private Color _contrastColor;
+        [SerializeField] private Gradient _colorBlinkingEffectGradient;
+        [SerializeField, Range(2, 10)] private float _animationLoopDuration = 2f;
 
         private Sequence _loadingAnimation;
 
@@ -21,9 +21,15 @@ namespace _Quarantine.Code.UI.Loading
             _loadingAnimation = DOTween.Sequence();
 
             _loadingAnimation
-                .Append(_gearImage.transform.DOLocalRotate(new Vector3(0, 0, 360), 2, RotateMode.FastBeyond360).SetEase(Ease.Linear))
-                .Join(_gearImage2.transform.DOLocalRotate(new Vector3(0, 0, -360), 2, RotateMode.FastBeyond360).SetEase(Ease.Linear))
-                .Join(_gearImage3.transform.DOLocalRotate(new Vector3(0, 0, 360), 2, RotateMode.FastBeyond360).SetEase(Ease.Linear))
+                .Append(_gearImage.transform.DOLocalRotate(new Vector3(0, 0, 360),
+                    2, RotateMode.FastBeyond360).SetEase(Ease.Linear))
+                .Join(_gearImage2.transform.DOLocalRotate(new Vector3(0, 0, -360 * 1.5f),
+                    2, RotateMode.FastBeyond360).SetEase(Ease.Linear))
+                .Join(_gearImage3.transform.DOLocalRotate(new Vector3(0, 0, 360 * 2f), 
+                    2, RotateMode.FastBeyond360).SetEase(Ease.Linear))
+                .Join(_gearImage.DOGradientColor(_colorBlinkingEffectGradient, _animationLoopDuration))
+                .Join(_gearImage2.DOGradientColor(_colorBlinkingEffectGradient, _animationLoopDuration - 0.5f))
+                .Join(_gearImage3.DOGradientColor(_colorBlinkingEffectGradient, _animationLoopDuration - 1f))
                 .SetLoops(-1)
                 .SetAutoKill(false);
         }
